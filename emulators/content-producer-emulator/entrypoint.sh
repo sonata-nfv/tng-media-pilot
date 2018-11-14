c017fe33216536c13c5c2b9e963cddd04e700923
ignacio <ignacio.dominguez.gomez.external@atos.net>
2018-11-14 10:57:15 +0100

    minor changes in the test runner

--- emulators/content-producer-emulator/entrypoint.sh
+++ emulators/content-producer-emulator/entrypoint.sh
@@ -2,5 +2,5 @@
 
 source variables.env
 
-FFREPORT=file=ffreport.log:repeat+verbose ffmpeg -loglevel repeat+verbose -stream_loop -1 -re -i /app/video1.mkv -c copy -f flv rtmp://$AGGREGATOR:1935/$APP/$STREAM > output.txt
+FFREPORT=file=ffreport.log:repeat+verbose ffmpeg -loglevel repeat+verbose -stream_loop -1 -re -i /app/video1.mkv -c copy -f flv rtmp://$AGGREGATOR:1935/$APP/$STREAM
 
--- video-test/Tests/runner.sh
+++ video-test/Tests/runner.sh
@@ -1,2 +1,2 @@
 docker pull ignaciodomin/media-cpe:dev
-docker run --rm -v cpe_vol:/tests ignaciodomin/media-cpe:dev ${workspace.absolutePath}/config.cfg
+docker run --rm -v tee:/workspace ignaciodomin/media-cpe:dev ${workspace.absolutePath}/config.cfg
