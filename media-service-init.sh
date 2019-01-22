58ea6f8eb79481b5261ad133a5c45cf4fc439942
ignacio <ignacio.dominguez.gomez.external@atos.net>
2019-01-22 11:39:52 +0100

    Service init script updated

--- media-service-init.sh
+++ media-service-init.sh
@@ -1,37 +1,42 @@
 #!/usr/bin/env bash
 
 #Create the wifi access-point
-create_ap w1p6s0 eno1 corewifi pacopaco --daemon
+#create_ap w1p6s0 eno1 corewifi pacopaco --daemon
 
-#Modify the iptables 
-iptables -t nat -A POSTROUTING -s 192.168.136.0/24 -o eno1 -j MASQUERADE
+#Modify the iptables
+#iptables -t nat -A POSTROUTING -s 192.168.136.0/24 -o eno1 -j MASQUERADE
+
+#Variables with the IPs of the aggregator, streaming engine and cms:
+MA="192.168.137.150:5000"
+MSE="192.168.137.151:1935"
+CMS="192.168.137.152:50000"
 
 #MEDIA SERVICE CONFIGURATION:
-#Service init config API (emulates the FSM function) 
+#Service init config API (emulates the FSM function)
 curl -H'content-Type:application/json' -X POST -d'{
     "Aggregators": [
         {
             "name": "Aggregator-001",
             "location": "Madrid",
-            "ip": "192.168.137.150:5000"
-        },
-        {
-            "name": "Aggregator-002",
-            "location": "Madrid",
-            "ip": "192.168.2.1:5000"
+            "ip": "'"$MA"'"
         }
     ],
     "StreamingEngines": [
         {
             "name": "Engine-001",
             "location": "Madrid",
-            "ip": "192.168.137.152:1935"
-        }
+            "ip": "'"$MSE"'"
+    	}
     ]
-}' http://192.168.137.150:50000/configure
+}' http://$CMS/configure
 
 #Register a camera:
-curl -H 'content-Type: application/json' -X POST -d '{"name":"cam1"}' http://192.168.137.152:50000/registerCamera
+curl -H 'content-Type: application/json' -X POST -d '{"name":"cam1"}' http://$CMS/registerCamera
+
+#Register a camera:
+curl -H 'content-Type: application/json' -X POST -d '{"name":"cam2"}' http://$CMS/registerCamera
+
+
 
 #Connect a client: 
-curl -H 'content-Type: application/json' -X GET -d '{"name":"cam1"}' http://192.168.137.152:50000/getStreamURL
+#curl -H 'content-Type: application/json' -X GET -d '{"name":"cam1"}' http://192.168.137.152:50000/getStreamURL
