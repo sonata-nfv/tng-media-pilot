b6ec76dac7ff8529611c86a510aa7f5ddd259b76
ignacio <ignacio.dominguez.gomez.external@atos.net>
2019-01-25 14:16:26 +0100

    Content consumer probe updated

--- emulators/content-consumer-emulator/entrypoint.sh
+++ emulators/content-consumer-emulator/entrypoint.sh
@@ -4,7 +4,7 @@ source variables.env
 
 echo "test"
 
-mpv http://$STREAMING_ENGINE:8080/hls/$STREAM.m3u8 --dump-stats=stats_test.txt --vo=null --ao=null --ao-null-untimed
+mpv http://$STREAMING_ENGINE:80/hls/$STREAM.m3u8 --dump-stats=stats_test.txt --vo=null --ao=null --ao-null-untimed
 #mpv http://192.168.137.72:8080/hls/demo1.m3u8
 
 
