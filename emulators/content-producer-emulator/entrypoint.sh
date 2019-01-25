be5de7fcc5885179e9e18cdb5e67259585b9e22d
ignacio <ignacio.dominguez.gomez.external@atos.net>
2019-01-25 10:34:46 +0100

    Content producer probe updated.

--- emulators/content-producer-emulator/entrypoint.sh
+++ emulators/content-producer-emulator/entrypoint.sh
@@ -6,4 +6,4 @@ source variables.env
 #ffmpeg -i /app/video1.mkv -deinterlace -c:v libx264 -pix_fmt yuv420p -s 1920x1080 -preset superfast -vb 6000k -r 30 -g 60 -bufsize 8000k -c:a aac -b:a 128k -ar 44100 -ac 2 -f flv rtmp://$AGGREGATOR:1935/$APP/$STREAM
 #FFREPORT=file=ffreport.log:repeat+verbose ffmpeg -loglevel repeat+verbose -stream_loop -1 -re -i /app/video1.mkv -deinterlace -vcodec libx264 -pix_fmt yuv420p -preset medium -r 30 -g 60 -b:v 6000k -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k -f flv rtmp://$AGGREGATOR:1935/$APP/$STREAM
 #FFREPORT=file=ffreport.log:repeat+verbose ffmpeg -loglevel repeat+verbose -stream_loop -1 -re -i /app/video1.mkv -c:v copy -c:a copy -g 60 -f flv rtmp://$AGGREGATOR:1935/live/$APP/$STREAM
-ffmpeg -loglevel repeat+verbose -stream_loop -1 -re -i /app/video1.mkv -acodec copy -vcodec copy -f flv rtmp://$AGGREGATOR:1935/cam1/cam1
+ffmpeg -loglevel repeat+verbose -stream_loop -1 -re -i /app/$VIDEO -f flv rtmp://$AGGREGATOR:1935/$APP/$STREAM
--- media-service-init.sh
+++ media-service-init.sh
@@ -7,9 +7,9 @@
 #iptables -t nat -A POSTROUTING -s 192.168.136.0/24 -o eno1 -j MASQUERADE
 
 #Variables with the IPs of the aggregator, streaming engine and cms:
-MA="192.168.137.150:5000"
-MSE="192.168.137.151:1935"
-CMS="192.168.137.152:50000"
+MA="172.31.13.44:5000"
+MSE="172.31.13.46:1935"
+CMS="172.31.13.45:50000"
 
 #MEDIA SERVICE CONFIGURATION:
 #Service init config API (emulates the FSM function)
@@ -33,12 +33,15 @@ curl -H'content-Type:application/json' -X POST -d'{
 
 
 #Register a camera:
-curl -H 'content-Type: application/json' -X POST -d '{"name":"cam1"}' http://$CMS/registerCamera
+curl -H 'content-Type: application/json' -X POST -d '{"name":"plane"}' http://$CMS/registerCamera
 
 #Register a camera:
-curl -H 'content-Type: application/json' -X POST -d '{"name":"cam2"}' http://$CMS/registerCamera
+curl -H 'content-Type: application/json' -X POST -d '{"name":"360"}' http://$CMS/registerCamera
 
 
 
 #Connect a client: 
-#curl -H 'content-Type: application/json' -X GET -d '{"name":"cam1"}' http://192.168.137.152:50000/getStreamURL
+curl -H 'content-Type: application/json' -X GET -d '{"name":"plane"}' http://$CMS/getStreamURL
+
+#Connect a client: 
+curl -H 'content-Type: application/json' -X GET -d '{"name":"360"}' http://$CMS/getStreamURL
