850d764fe7453cc0d4c963ac55df0b6d79851703
ignacio <ignacio.dominguez.gomez.external@atos.net>
2019-04-24 10:26:18 +0200

    Probes code updated

--- test-probes/ffprobe/entrypoint.sh
+++ test-probes/ffprobe/entrypoint.sh
@@ -2,4 +2,27 @@
 
 source variables.env
 
-ffprobe -show_packets -select_streams v -print_format json rtmp://$STREAMING_ENGINE:1935/live/$STREAM > /output/ffprobe/logs.txt | grep -n K_ -B 10
+mkdir -p /output/ffprobe/${HOSTNAME}
+
+# Start the ffmpeg process:
+ffprobe -show_frames -select_streams v -print_format json -i "rtmp://${STREAMING_ENGINE}:1935/live/${STREAM}" > /output/ffprobe/${HOSTNAME}/logs.txt &
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start ffprobe: $status"
+  exit $status
+fi
+
+# Naive check runs checks once a minute to see if either of the processes exited.
+# This illustrates part of the heavy lifting you need to do if you want to run
+# more than one service in a container. The container exits with an error
+# if it detects that either of the processes has exited.
+# Otherwise it loops forever, waking up every 60 seconds
+
+while sleep 1; do
+  ps aux | grep ffprobe | grep -q -v grep
+  PROCESS_1_STATUS=$?
+  if [ $PROCESS_1_STATUS -ne 0 ]; then
+    echo "One of the processes has already exited."
+    exit 0
+  fi
+done
