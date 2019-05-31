b67d4fac4a9c226986ced3f66378c775b14e539d
ignacio <ignacio.dominguez.gomez.external@atos.net>
2019-05-31 09:47:49 +0200

    Initiator test-probe updated to work with the new version of the CMS

--- test-probes/initiator/entrypoint.sh
+++ test-probes/initiator/entrypoint.sh
@@ -3,29 +3,10 @@
 source variables.env
 
 #MEDIA SERVICE CONFIGURATION:
-#Service init config API (emulates the FSM function)
-curl -H'content-Type:application/json' -X POST -d'{
-    "Aggregators": [
-        {
-            "name": "MA-001-VnV",
-            "location": "VnV",
-            "ip": "'"$MA"':5000"
-        }
-    ],
-    "StreamingEngines": [
-        {
-            "name": "MSE-001-VnV",
-            "location": "VnV",
-            "ip": "'"$MSE"'"
-        }
-    ]
-
-}' http://$CMS:50000/configure
-
 
 #Register a camera:
 #It is possible to register multiple cameras, just put a curl per camera
-curl -H 'content-Type: application/json' -X POST -d '{"name":"'"$CAMERA"'"}' http://$CMS:50000/registerCamera
+curl -H 'content-Type: application/json' -X POST -d '{"name":"'"$CAMERA"'","type":"plane"}' http://$CMS:50000/registerCamera
 
 #Connect a client: 
 #It is possible to connect multiple cameras, just put a curl per streaming-engine
